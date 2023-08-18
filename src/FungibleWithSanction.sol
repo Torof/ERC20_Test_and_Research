50d8ddd2b663a1660a9907e737a07d5659c63f4d
torof <scottpanam@protonmail.com>
2023-08-18 23:44:46 +0900

    FungibleWithSanction 100% coverage

--- src/FungibleWithSanction.sol
+++ src/FungibleWithSanction.sol
@@ -2,4 +2,72 @@
 
 pragma solidity 0.8.18;
 
-contract FungibleWithSanction {}
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract FungibleWithSanction is ERC20 {
+    address public immutable admin;
+
+    mapping(address => bool) private _isBlacklisted;
+
+    error SendingFromBlacklisted(address);
+    error SendingToBlacklisted(address);
+    error Blacklisted(address);
+    error NotBlacklisted(address);
+    error NotAdmin();
+
+    modifier onlyAdmin() {
+        if(msg.sender != admin) revert NotAdmin();
+        _;
+    }
+
+    constructor(
+        address admin_,
+        string memory name,
+        string memory symbol
+    ) ERC20(name, symbol) {
+        admin = admin_;
+        _mint(admin, 10_000_000);
+    }
+
+    function addToBlacklist(address toBlacklist) public onlyAdmin {
+        if(_isBlacklisted[toBlacklist]) revert Blacklisted(toBlacklist);
+        _isBlacklisted[toBlacklist] = true;
+    }
+
+    function removeFromBlacklist(address toRemoveFromBlacklist) public onlyAdmin {
+        if(!_isBlacklisted[toRemoveFromBlacklist]) revert NotBlacklisted(toRemoveFromBlacklist);
+        _isBlacklisted[toRemoveFromBlacklist] = false;
+    }
+
+    function _transfer(address from, address to, uint256 amount) internal override {
+        if (_isBlacklisted[from])
+            revert SendingFromBlacklisted(from);
+        if (_isBlacklisted[to]) revert SendingToBlacklisted(to);
+        super._transfer(from, to, amount);
+    }
+
+
+    function isBlacklisted(address toCheck) public view returns(bool){
+        return _isBlacklisted[toCheck];
+    }
+}
+
+
+// function transfer(
+    //     address to,
+    //     uint256 amount
+    // ) public override returns (bool) {
+    //     if (_isBlacklisted[msg.sender])
+    //         revert SendingFromBlacklisted(msg.sender);
+    //     if (_isBlacklisted[to]) revert SendingToBlacklisted(to);
+    //     super.transfer(to, amount);
+    //     return true;
+    // }
+
+    // function transferFrom(address from, address to, uint256 amount) public override returns(bool){
+    //     if (_isBlacklisted[from])
+    //         revert SendingFromBlacklisted(from);
+    //     if (_isBlacklisted[to]) revert SendingToBlacklisted(to);
+    //     super.transferFrom(from, to, amount);
+    //     return true;
+    // }
--- test/FungibleWithSanction.t.sol
+++ test/FungibleWithSanction.t.sol
@@ -2,4 +2,246 @@
 
 pragma solidity 0.8.18;
 
-contract FungibleWithSanction {}
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "../src/FungibleWithSanction.sol";
+
+contract FungibleWithSanctionTest is Test {
+    FungibleWithSanction public funWiSan;
+    address immutable public admin = vm.addr(1);
+    address immutable public sender1 = vm.addr(2);
+    address immutable public senderBlacklisted = vm.addr(3);
+    address immutable public receiver1 = vm.addr(4);
+    address immutable public receiverBlacklisted = vm.addr(5);
+    
+    function setUp() public {
+        funWiSan =  new FungibleWithSanction(admin, "NoPowerToken", "NPT");
+    }
+
+    function testSetUp() public {
+        assertEq(funWiSan.name(), "NoPowerToken");
+        assertEq(funWiSan.symbol(), "NPT");
+        assertEq(funWiSan.admin(), admin);
+        assertEq(funWiSan.balanceOf(admin), 10_000_000);
+        assertEq(funWiSan.totalSupply(), 10_000_000);
+    }
+
+    function testWithFuzzingAddToBlacklist(address blacklisted) public {
+        //address should not be blacklisted
+        assertFalse(funWiSan.isBlacklisted(blacklisted));
+
+        //admin blacklists address
+        vm.prank(admin);
+        funWiSan.addToBlacklist(blacklisted);
+
+        //address should be blacklisted
+        assertTrue(funWiSan.isBlacklisted(blacklisted));
+    }
+
+    function testRevertWithFuzzingAddToBlacklistIfBlacklisted(address blacklistedFuzz) public {
+        //admin blacklists address
+        vm.startPrank(admin);
+        funWiSan.addToBlacklist(blacklistedFuzz);
+
+        //Should revert , user already blacklisted
+        bytes4 selector = bytes4(keccak256("Blacklisted(address)"));
+        vm.expectRevert(abi.encodeWithSelector(selector, blacklistedFuzz ));
+        funWiSan.addToBlacklist(blacklistedFuzz);
+        vm.stopPrank();
+    }
+
+    function testRevertAddToBlacklistIfNotAdmin(address notAdminFuzz) public {
+        vm.assume(notAdminFuzz != admin);
+
+        //caller is not admin
+        vm.startPrank(notAdminFuzz);
+        bytes4 selector = bytes4(keccak256("NotAdmin()"));
+
+        //should revert notAdmin()
+        vm.expectRevert(selector);
+        funWiSan.addToBlacklist(senderBlacklisted);
+        vm.stopPrank();
+    }
+
+    function testWithFuzzingRemoveFromBlacklist(address blacklistedFuzz) public {
+        //admin blacklists address
+        vm.startPrank(admin);
+        funWiSan.addToBlacklist(blacklistedFuzz);
+
+        //address should be blacklisted
+        assertTrue(funWiSan.isBlacklisted(blacklistedFuzz));
+
+        //admin removes from blacklist
+        funWiSan.removeFromBlacklist(blacklistedFuzz);
+
+        vm.stopPrank();
+
+        //address should not be blacklisted anymore
+        assertFalse(funWiSan.isBlacklisted(blacklistedFuzz));
+    }
+
+    function testRevertRemoveFromBlacklistIfNotBlacklisted(address blacklistedFuzz) public {
+        vm.startPrank(admin);
+
+        //Should revert , user already blacklisted
+        bytes4 selector = bytes4(keccak256("NotBlacklisted(address)"));
+        vm.expectRevert(abi.encodeWithSelector(selector, blacklistedFuzz ));
+        funWiSan.removeFromBlacklist(blacklistedFuzz);
+        vm.stopPrank();
+    }
+
+    function testWithFuzzingRevertRemoveFromBlacklistIfNotAdmin(address notAdminFuzz) public {
+        vm.assume(notAdminFuzz != admin);
+
+        vm.prank(admin);
+        //admin adds to blacklist
+        funWiSan.addToBlacklist(senderBlacklisted);
+
+        //caller is not admin
+        vm.startPrank(notAdminFuzz);
+        bytes4 selector = bytes4(keccak256("NotAdmin()"));
+
+        //should revert notAdmin()
+        vm.expectRevert(selector);
+        funWiSan.removeFromBlacklist(senderBlacklisted);
+        vm.stopPrank();
+    }
+
+    function testIsBlacklisted(address blacklisted) public {
+        //address should not be blacklisted
+        assertFalse(funWiSan.isBlacklisted(blacklisted));
+
+        //admin blacklists address
+        vm.prank(admin);
+        funWiSan.addToBlacklist(blacklisted);
+
+        //address should be blacklisted
+        assertTrue(funWiSan.isBlacklisted(blacklisted));
+    }
+
+    function testWithFuzzingTransfer(address senderFuzz) public {
+        vm.assume(senderFuzz != address(0));
+        vm.assume(senderFuzz != admin);
+
+        uint balanceSFBefore = funWiSan.balanceOf(senderFuzz);
+        uint balanceAdminBefore = funWiSan.balanceOf(admin);
+
+        //admin transfer 1_000_000 to senderFuzz
+        vm.prank(admin);
+        bool transferExecuted = funWiSan.transfer(senderFuzz, 1_000_000);
+
+        //assert transfer was successful
+        assertTrue(transferExecuted);
+
+        uint balanceSFAfter = funWiSan.balanceOf(senderFuzz);
+        uint balanceAdminAfter = funWiSan.balanceOf(admin);
+        //assert balances were updated
+        assertEq(balanceSFAfter, balanceSFBefore + 1_000_000);
+        assertEq(balanceAdminAfter, balanceAdminBefore - 1_000_000);
+    }
+
+    
+    function testTransferFrom(address fromFuzz, address toFuzz) public {
+        vm.assume(fromFuzz != address(0));
+        vm.assume(toFuzz != address(0));
+        vm.assume(toFuzz != fromFuzz);
+        deal(address(funWiSan), fromFuzz, 1_000_000);
+        uint balanceSenderBefore = funWiSan.balanceOf(fromFuzz);
+        uint balanceReceiverBefore = funWiSan.balanceOf(toFuzz);
+
+        vm.prank(fromFuzz);
+        funWiSan.approve(admin, 1_000_000);
+
+        //admin transfer 1_000_000 to sender1
+        vm.prank(admin);
+        bool transferExecuted = funWiSan.transferFrom(fromFuzz, toFuzz, 1_000_000);
+
+        //assert transfer was successful
+        assertTrue(transferExecuted);
+
+        uint balanceSenderAfter = funWiSan.balanceOf(fromFuzz);
+        uint balanceReceiverAfter = funWiSan.balanceOf(toFuzz);
+
+        //assert balances were updated
+        assertEq(balanceSenderAfter, balanceSenderBefore - 1_000_000);
+        assertEq(balanceReceiverAfter, balanceReceiverBefore + 1_000_000);
+    }
+
+    function testWithFuzzingRevertTransferIfSenderBlacklisted(address blacklistedSender) public {
+        vm.assume(blacklistedSender != address(0));
+        deal(address(funWiSan), blacklistedSender, 1_000_000);
+
+        //admin adds address to blacklist
+        vm.prank(admin);
+        funWiSan.addToBlacklist((blacklistedSender));
+
+        vm.prank(blacklistedSender);
+        bytes4 selector = bytes4(keccak256("SendingFromBlacklisted(address)"));
+        //transfer should revert, sender address is blacklisted
+        vm.expectRevert(abi.encodeWithSelector(selector, blacklistedSender));
+        funWiSan.transfer(receiver1, 1_000_000);
+    }
+
+    function testRevertTransferIfReceiverBlacklisted(address blacklistedReceiver) public {
+        vm.assume(blacklistedReceiver != address(0));
+        vm.assume(blacklistedReceiver != sender1);
+
+        //deals token to address for transfer
+        deal(address(funWiSan), sender1 , 1_000_000);
+
+        //admin adds address to blacklist
+        vm.prank(admin);
+        funWiSan.addToBlacklist((blacklistedReceiver));
+
+        vm.prank(sender1);
+        bytes4 selector = bytes4(keccak256("SendingToBlacklisted(address)"));
+        //transfer should revert, receiver address is blacklisted
+        vm.expectRevert(abi.encodeWithSelector(selector, blacklistedReceiver));
+        funWiSan.transfer(blacklistedReceiver, 1_000_000);
+    }
+
+    function testRevertTransferFromIfSenderBlacklisted(address blacklistedSender, address senderOnBehalf) public {
+        vm.assume(blacklistedSender != address(0));
+        vm.assume(senderOnBehalf != address(0));
+        vm.assume(senderOnBehalf != blacklistedSender);
+        deal(address(funWiSan), blacklistedSender, 1_000_000);
+
+        //admin adds address to blacklist
+        vm.prank(admin);
+        funWiSan.addToBlacklist((blacklistedSender));
+
+        //sender approves random address to transfer on behalf
+        vm.prank(blacklistedSender);
+        funWiSan.approve(senderOnBehalf, 1_000_000);
+
+        //random address transfers from blacklisted sender, should revert
+        vm.prank(senderOnBehalf);
+        bytes4 selector = bytes4(keccak256("SendingFromBlacklisted(address)"));
+        //transfer should revert, sender address is blacklisted
+        vm.expectRevert(abi.encodeWithSelector(selector, blacklistedSender));
+        funWiSan.transferFrom(blacklistedSender, receiver1, 1_000_000);
+    }
+
+    function testRevertTransferFromIfReceiverBlacklisted(address blacklistedReceiver, address senderOnBehalf) public {
+        vm.assume(blacklistedReceiver != address(0));
+        vm.assume(senderOnBehalf != address(0));
+
+        //deals token to address for transfer
+        deal(address(funWiSan), sender1 , 1_000_000);
+
+        //admin adds address to blacklist
+        vm.prank(admin);
+        funWiSan.addToBlacklist((blacklistedReceiver));
+
+        //sender approves random address to transfer on behalf
+        vm.prank(sender1);
+        funWiSan.approve(senderOnBehalf, 1_000_000);
+
+        //random allowed address transfers to blacklisted, should revert
+        vm.prank(senderOnBehalf);
+        bytes4 selector = bytes4(keccak256("SendingToBlacklisted(address)"));
+        //transfer should revert, receiver address is blacklisted
+        vm.expectRevert(abi.encodeWithSelector(selector, blacklistedReceiver));
+        funWiSan.transferFrom(sender1, blacklistedReceiver, 1_000_000);
+    }
+}
